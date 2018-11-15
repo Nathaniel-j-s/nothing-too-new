23fbf2ba9f2827a0507468c6a2ec2e16be040991
Nathaniel-j-s <nathaniel.j.mith@gmail.com>
2018-11-14 23:53:30 -0700

    Adding new file for factions and relations

---
+++ anima2factions.js
@@ -0,0 +1,13 @@
+class Faction {
+  constructor(config) {
+    this.factionName = config.factionName || 'Solo';
+    this.standing = config.standing;
+    this.belief = config.belief;
+    this.relations = config.relations; // This should be an array of objects consisting of a factionName and degree (which will range from -100 to 100).
+  }
+}
+
+// A fellow faction member's relations can be modified by a character with a standing of 25 or higher.
+// A character's faction relations may only be modified if their belief is above 25 however.
+// The higher an influencer's standing combines with the character's belief to create their modification.
+// This can result in the character being affected further than intended by the influencer.
