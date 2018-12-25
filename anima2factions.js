662ad8269020d4dd6592248e8fec412b300a3a67
Nathaniel-j-s <nathaniel.j.mith@gmail.com>
2018-12-25 14:21:16 -0700

    refigured opinion system

--- anima2factions.js
+++ anima2factions.js
@@ -1,12 +1,11 @@
-class Faction {
+class Opinion {
   constructor(config) {
-    this.factionName = config.factionName || 'Solo';
-    this.standing = config.standing;
-    this.belief = config.belief;
-    this.relations = config.relations; // This should be an array of objects consisting of a factionName and degree (which will range from -100 to 100).
+    this.opinionName = config.opinionName
+    this.level = config.level;
+    this.factors = config.factors; // This is an array of objects which include a time/date, a modifier, and a string with the reason.
   }
-  influence(influencer, targetFaction) {
-    // This is where this should go.
+  influence(amount) {
+    this.level += amount;
   }
 }
 
@@ -15,12 +14,6 @@ class Relationship {
     this.char = config.char;
     this.level = config.level;
     this.relationshipType = config.relationshipType;
-  }
-  increase(amount) {
-    this.level += amount;
-  }
-  decrease(amount) {
-    this.level -= amount;
   }
   changeType(type) {
     this.relationshipType = type;
@@ -44,24 +37,13 @@ function generateRelationship(charName, level, special) {
   return new Relationship(c);
 }
 
+function compareOpinions() {
+  
+}
+
 function determineInitialRelationshipLevel() {
   // This function will compare visual factions of a character against another character's opinions.
 }
-
-class Opinion {
-  constructor(config) {
-    this.subject = config.subject;
-    this.level = config.level;
-    this.factors = config.factors; // This was originally going to be in a separate class, Knowledge.
-  }
-}
-
-// A fellow faction member's relations can be modified by a character with a standing of 25 or higher.
-// A character's faction relations may only be modified if their belief is above 25 however.
-// The higher an influencer's standing combines with the character's belief to create their modification.
-// This can result in the character being affected further than intended by the influencer.
-
-// May require a full rework. Something about this seems like it won't fit in the overall scheme of the game.
 
 // Okay. So what we need is a system for personal relationships, relationships to factions, and a way to be identified by faction.
 // Personal relationships will affect: Aggression/peace on sight, relationship to any factions displayed.
