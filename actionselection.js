43c21fd695b6c11dd5be78b8753092871f070da8
Nathaniel-j-s <nathaniel.j.mith@gmail.com>
2019-01-17 13:17:43 -0700

    lol better idea

--- actionselection.js
+++ actionselection.js
@@ -1,6 +1,8 @@
 function createAction() {
   let actionType = action;
   let performer = performer;
-  let target = target; // How do we best get the information here?
+  let target = target;
   let final = `${actionType}(${performer}, ${target})`;
 }
+
+// Nope. This was dumb. Don't do this.
